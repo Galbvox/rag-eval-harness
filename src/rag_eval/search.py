5923910806e9277ae167c835df1c1869f94c55f6
Gal <galbvox@gmail.com>
2026-08-13 13:00:59 +0300

    fix search

--- src/rag_eval/search.py
+++ src/rag_eval/search.py
@@ -1,22 +1,22 @@
-#Closure = function that remember value from the created place
+IGNORED = ["the", "is", "that", "a", "of", "what"]
+
 def make_search(chunks):
     def search(query, k):
         words = query.lower().replace("?", " ").split()  
         scored = []    
         for chunk in chunks:
             count = 0
+            chunk_words = chunk.lower().replace(".", " ").replace("?", " ").split()
             for word in words:
-                if word in  chunk.lower().replace(".", " ").replace("?", " ").split():
-                    count = count + 1
+                if word in IGNORED:
+                    continue
+                for cw in chunk_words:                 
+                    if cw.startswith(word):
+                       count = count + 1
+                       break
             
             scored.append((count, chunk))
         scored.sort(reverse=True)
         result = [chunk for count, chunk in scored[:k]]
         return result
     return search
-    
-
-    
-            
-
-    
